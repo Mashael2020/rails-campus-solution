efd7142f9cc3f3d26fff1fe32993b58e2aee5905
micfin <michael.finneran@generalassemb.ly>
2019-05-19 14:35:37 +0300

    Add Books

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    get "/books", to: "books#index", as: "books" # books_path = /books 
+    get '/books/new', to: "books#new", as: "new_book" # new_book_path = /books/new
+    get '/books/:id/edit', to: "books#edit", as: "edit_book" # edit_book_path(@book)
+    get "/books/:id", to: "books#show", as: "book" # book_path(book) = /books/:id
+    delete '/books/:id', to: "books#destroy"
+    post '/books', to: "books#create"
+    patch '/books/:id', to: "books#update"
+end
