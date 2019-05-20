58fca4bda9a1670330845ad5225935f4144b20c7
micfin <michael.finneran@generalassemb.ly>
2019-05-20 11:15:06 +0300

    Remove author from books
    Add migration file to update schema
    Remove author from controller params
    Update the views

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -36,6 +36,6 @@ class BooksController < ApplicationController
     end
 
     def book_params
-        params.require(:book).permit(:title, :author)
+        params.require(:book).permit(:title)
     end
 end
