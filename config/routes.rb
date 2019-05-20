380888c367828f1f0877d0a680f20d02ead6a3d2
micfin <michael.finneran@generalassemb.ly>
2019-05-20 14:46:48 +0300

    Add relationship between books and authors

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-    get "/books", to: "books#index", as: "books" # books_path = /books 
-    get '/books/new', to: "books#new", as: "new_book" # new_book_path = /books/new
-    get '/books/:id/edit', to: "books#edit", as: "edit_book" # edit_book_path(@book)
-    get "/books/:id", to: "books#show", as: "book" # book_path(book) = /books/:id
-    delete '/books/:id', to: "books#destroy"
-    post '/books', to: "books#create"
-    patch '/books/:id', to: "books#update"
+    # get "/books", to: "books#index", as: "books" # books_path = /books 
+    # get '/books/new', to: "books#new", as: "new_book" # new_book_path = /books/new
+    # get '/books/:id/edit', to: "books#edit", as: "edit_book" # edit_book_path(@book)
+    # get "/books/:id", to: "books#show", as: "book" # book_path(book) = /books/:id
+    # delete '/books/:id', to: "books#destroy"
+    # post '/books', to: "books#create"
+    # patch '/books/:id', to: "books#update"
     
-    get '/authors', to: 'authors#index', as: 'authors'
-    get '/authors/new', to: "authors#new", as: "new_author" 
-    get '/authors/:id/edit', to: "authors#edit", as: "edit_author"
-    get '/authors/:id', to: 'authors#show', as: 'author'
-    delete '/authors/:id', to: "authors#destroy"
-    post '/authors', to: "authors#create"
-    patch '/authors/:id', to: "authors#update"
+    resources :authors do 
+      resources :books
+    end
+    # get '/authors', to: 'authors#index', as: 'authors'
+    # get '/authors/new', to: "authors#new", as: "new_author" 
+    # get '/authors/:id/edit', to: "authors#edit", as: "edit_author"
+    # get '/authors/:id', to: 'authors#show', as: 'author'
+    # delete '/authors/:id', to: "authors#destroy"
+    # post '/authors', to: "authors#create"
+    # patch '/authors/:id', to: "authors#update"
 end
